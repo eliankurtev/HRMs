d03a53bf4b28395f2ef37ad8937165da18bc02bf
Radoslav Jivakov <r.jivakov@gmail.com>
2019-06-22 14:09:34 +0300

    adding storage service

--- src/main/java/com/hr/hrsystem/HrSystemApplication.java
+++ src/main/java/com/hr/hrsystem/HrSystemApplication.java
@@ -1,14 +1,27 @@
 package com.hr.hrsystem;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hr.hrsystem.service.impl.StorageService;
+
 @SpringBootApplication
-public class HrSystemApplication {
+public class HrSystemApplication implements CommandLineRunner {
+
+	@Resource
+	StorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrSystemApplication.class, args);
 	}
 
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
 }
 
